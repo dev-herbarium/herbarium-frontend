3770fff86160f176559305f341953b23b35deb40
Gabriel ML <gabrielmljob@gmail.com>
2025-11-03 09:54:51 +0100

    feat: Add `BEM-based CSS styles` for Home Components
    Implement structured CSS using BEM methodology for Homepage Components
    with consistent naming conventions.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,25 +2,31 @@
 
 import { useState } from "react";
 import RegistrationForm from "../../components/Registration/RegistrationForm";
+import './Home.css';
 
 /**
- * <b> Home Page Component</b>
+ * <b>Home Page Component</b>
  * 
  * @function Home
- * @description <b> The landing page for the application. </b>
+ * @description <b>The landing page for the application.</b>
  * @returns {JSX.Element} The Home page content.
  * @component
  */
 function Home() {
   const [showRegistration, setShowRegistration] = useState(false);
 
+  const buttonClass = `home__button ${ showRegistration ? "home__button--active" : "" }`;
+
   return (
-    <div data-testid="home-page">
-      <h1>Welcome to Herbarium! 🌱</h1>
+    <div className="home" data-testid="home-page">
+      <h1 className="home__title">Welcome to Herbarium! 🌱</h1>
       <p>Start exploring plants and managing your recipes.</p>
 
-      <button onClick={() => setShowRegistration(!showRegistration)}>
-        {showRegistration ? "Hide Registration" : "Register User"}
+      <button 
+        className={buttonClass} 
+        onClick={() => setShowRegistration(!showRegistration)}
+        >
+          {showRegistration ? "Hide Registration" : "Register User"}
       </button>
 
       {showRegistration && <RegistrationForm />}
